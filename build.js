c84d5e2e97fcaf1b1d1bff80ba8862a63657824f
travisvroman <travisvroman@gmail.com>
2019-04-17 00:11:04 -0400

    Fixed build process for Linux

--- build.js
+++ build.js
@@ -9,6 +9,9 @@ fsExtra.emptyDirSync("dist");
 
 console.log("Building...");
 try {
+    cp.execSync("node node_modules/typescript/bin/tsc", {
+        stdio: "inherit"
+    });
     console.log("\x1b[35m***BUILD COMPLETE***\x1b[0m");
 } catch (ex) {
     console.log(ex.stdout);
